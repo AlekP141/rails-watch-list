b88ee734330d3bf239241d5ed384b8e62f27f169
Alek Pialucha <pialuchaa@btinternet.com>
2023-05-26 14:11:46 +0100

    changed root

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  root "lists#index"
+
   resources :movies
   resources :lists do
     resources :bookmarks, only: [:new, :create]
